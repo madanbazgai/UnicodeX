c350ce1a45ad49ebe4a7fd5265740dc4193a9052
Madan Bajgai <madanbajgai@gmail.com>
2023-04-20 15:55:32 +0545

    minor update for pwa

--- public/sw.js
+++ public/sw.js
@@ -1,38 +1,47 @@
-const CACHE_NAME = 'my-pwa-cache';
-const urlsToCache = [
-];
+// This is the "Offline page" service worker
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    // serve from cache
-                    return response;
-                } else {
-                    // fetch from network
-                    return fetch(event.request)
-                        .then(response => {
-                            // cache new response
-                            return caches.open(CACHE_NAME)
-                                .then(cache => {
-                                    cache.put(event.request, response.clone());
-                                    return response;
-                                });
-                        })
-                        .catch(error => {
-                            console.error('Fetch failed:', error);
-                            // serve fallback response
-                            return caches.match('/offline.html');
-                        });
-                }
-            })
-    );
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
